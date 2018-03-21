0a9c175a349df201ff38daa7e3c5baf64b5608d9
Pete <swanseatechsupport@hotmail.co.uk>
2018-03-21 16:57:46 +0000

    Separated PaperSize object from PaperSize lookup

---
+++ src/uk/gov/dvla/osg/common/classes/PaperSize.java
@@ -0,0 +1,15 @@
+package uk.gov.dvla.osg.common.classes;
+
+public class PaperSize {
+
+	private double multiplier;
+
+	public PaperSize(double multiplier) {
+		this.multiplier = multiplier;
+	}
+
+	public double getMultiplier() {
+		return multiplier;
+	}
+
+}
