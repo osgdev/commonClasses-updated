e6947e3a28fd901fe10cde2ab46a8c8d988776e9
Pete <swanseatechsupport@hotmail.co.uk>
2018-03-21 17:29:55 +0000

    Refactored SelectorLookup as singleton

--- src/uk/gov/dvla/osg/common/config/SelectorLookup.java
+++ src/uk/gov/dvla/osg/common/config/SelectorLookup.java
@@ -7,42 +7,65 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import uk.gov.dvla.osg.common.classes.Selector;
 
-
 public class SelectorLookup {
-	private static final Logger LOGGER = LogManager.getLogger();	
+	private static final Logger LOGGER = LogManager.getLogger();
 	private HashMap<String, Selector> lookup = new HashMap<>();
-	
-	
-	public SelectorLookup(String file){
-		if(new File(file).exists()){
-			try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-				String line;
-				
-			    while ((line = br.readLine()) != null) {
-			    	String[] array = line.split("\\|");
-			    	if( !("SELECTOR".equals(array[0].trim())) ){
-			    		lookup.put(array[0].trim(), new Selector(array[1].trim(),
-			                    							     array[2].trim(),
-			                    							     array[3].trim()) );
-			    	}
-			    }
-			} catch (FileNotFoundException e) {
-				LOGGER.fatal("Lookup file error: '{}'",e.getMessage());
-				System.exit(1);
-			} catch (IOException e) {
-				LOGGER.fatal("Lookup file error: '{}'",e.getMessage());
-				System.exit(1);
-			} catch (NullPointerException e){
-				LOGGER.fatal("Lookup file error: '{}'",e.getMessage());
-				System.exit(1);
+
+	/******************************************************************************************
+	 * SINGLETON PATTERN
+	 ******************************************************************************************/
+	private static String filename;
+
+	private static class SingletonHelper {
+		private static final SelectorLookup INSTANCE = new SelectorLookup();
+	}
+
+	public static SelectorLookup getInstance() {
+		if (StringUtils.isBlank(filename)) {
+			throw new RuntimeException("Selector Lookup not initialised before use");
+		}
+		return SingletonHelper.INSTANCE;
+	}
+
+	public static void init(String file) throws RuntimeException {
+		if (StringUtils.isBlank(filename)) {
+			if (new File(file).isFile()) {
+				filename = file;
+			} else {
+				throw new RuntimeException("Selector Lookup file " + filename + " does not exist on filepath.");
 			}
-		}else{
-			LOGGER.fatal("Lookup file: '{}' doesn't exist",file);
+		} else {
+			throw new RuntimeException("Selector has already been initialised");
+		}
+	}
+
+	/*****************************************************************************************/
+
+	public SelectorLookup() {
+
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+			String line;
+
+			while ((line = br.readLine()) != null) {
+				String[] array = line.split("\\|");
+				if (!("SELECTOR".equals(array[0].trim()))) {
+					lookup.put(array[0].trim(), new Selector(array[1].trim(), array[2].trim(), array[3].trim()));
+				}
+			}
+		} catch (FileNotFoundException e) {
+			LOGGER.fatal("Lookup file error: '{}'", e.getMessage());
+			System.exit(1);
+		} catch (IOException e) {
+			LOGGER.fatal("Lookup file error: '{}'", e.getMessage());
+			System.exit(1);
+		} catch (NullPointerException e) {
+			LOGGER.fatal("Lookup file error: '{}'", e.getMessage());
 			System.exit(1);
 		}
 	}
