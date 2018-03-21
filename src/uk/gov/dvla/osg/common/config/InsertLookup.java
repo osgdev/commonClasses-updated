28a60a071d16d84d270310e1e9722908fc3b6327
Pete <swanseatechsupport@hotmail.co.uk>
2018-03-21 17:59:13 +0000

    Refactored InsertLookup as singleton

--- src/uk/gov/dvla/osg/common/config/InsertLookup.java
+++ src/uk/gov/dvla/osg/common/config/InsertLookup.java
@@ -1,11 +1,13 @@
 package uk.gov.dvla.osg.common.config;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,10 +18,39 @@ public class InsertLookup {
 	private static final Logger LOGGER = LogManager.getLogger();
 	private HashMap<String, Insert> lookup = new HashMap<>();
 	
-	public InsertLookup(String filePath) {
+	   /******************************************************************************************
+     *              SINGLETON PATTERN
+     ******************************************************************************************/
+    private static String filename;
+
+    private static class SingletonHelper {
+        private static final InsertLookup INSTANCE = new InsertLookup();
+    }
+
+    public static InsertLookup getInstance() {
+        if (StringUtils.isBlank(filename)) {
+            throw new RuntimeException("Insert Lookup not initialised before use");
+        }
+        return SingletonHelper.INSTANCE;
+    }
+
+    public static void init(String file) throws RuntimeException {
+        if (StringUtils.isBlank(filename)) {
+            if (new File(file).isFile()) {
+                filename = file;
+            } else {
+                throw new RuntimeException("Insert Lookup File " + filename + " does not exist on filepath.");
+            }
+        } else {
+            throw new RuntimeException("Insert Lookup has already been initialised");
+        }
+    }
+    /*****************************************************************************************/
+    
+	private InsertLookup() {
 		
 		LOGGER.trace("Creating Insert Lookup..");
-		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
 			String line;
 		    while ((line = br.readLine()) != null) {
 		    	String[] array = line.split(",");
