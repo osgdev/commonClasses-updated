e27896aea4d3244d06be48a2202e516d0fd52b4a
Pete <swanseatechsupport@hotmail.co.uk>
2018-03-21 17:50:21 +0000

    Refactored PapersizeLookup as singleton

--- src/uk/gov/dvla/osg/common/config/PapersizeLookup.java
+++ src/uk/gov/dvla/osg/common/config/PapersizeLookup.java
@@ -1,11 +1,13 @@
 package uk.gov.dvla.osg.common.config;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -15,9 +17,38 @@ public class PapersizeLookup {
 	private static final Logger LOGGER = LogManager.getLogger();
 	private HashMap<String, PaperSize> lookup = new HashMap<>();
 	
-	public PapersizeLookup(String filePath){
+    /******************************************************************************************
+     *              SINGLETON PATTERN
+     ******************************************************************************************/
+    private static String filename;
+
+    private static class SingletonHelper {
+        private static final PapersizeLookup INSTANCE = new PapersizeLookup();
+    }
+
+    public static PapersizeLookup getInstance() {
+        if (StringUtils.isBlank(filename)) {
+            throw new RuntimeException("Papersize Lookup not initialised before use");
+        }
+        return SingletonHelper.INSTANCE;
+    }
+
+    public static void init(String file) throws RuntimeException {
+        if (StringUtils.isBlank(filename)) {
+            if (new File(file).isFile()) {
+                filename = file;
+            } else {
+                throw new RuntimeException("Papersize Lookup File " + filename + " does not exist on filepath.");
+            }
+        } else {
+            throw new RuntimeException("Papersize Lookup has already been initialised");
+        }
+    }
+    /*****************************************************************************************/
+    
+	private PapersizeLookup() {
 		LOGGER.info("Creating Papersize Lookup..");
-		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
 			String line;
 		    while ((line = br.readLine()) != null) {
 		    	if( !(line.startsWith("#")) ){
